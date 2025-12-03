093c7ebbbd256c04291c9609075f371842e2dc3d
Benjamin Rubio <benjar@BenjaR---MacBook-Pro-6.local>
2025-12-03 10:47:37 -0500

    deleted old files

--- src/consts.ts
+++ src/consts.ts
@@ -16,10 +16,6 @@ export const NAV_LINKS: SocialLink[] = [
     href: '/blog',
     label: 'blog',
   },
-  {
-    href: '/about',
-    label: 'about',
-  },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
